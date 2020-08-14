35989aeb8e1cc3856a2d1e485070068353cf83ae
arrrlekino <zebrasteel@gmail.com>
2020-08-14 17:17:03 +0200

    add seeds, bootstrap, style...

--- Rails_2/db/schema.rb
+++ Rails_2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_160415) do
+ActiveRecord::Schema.define(version: 2020_08_13_162123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_08_07_160415) do
   create_table "friends", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
